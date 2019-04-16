2071b454c5a0624a789c93800c94a2316fe4a20e
Simon Hampe <simon.hampe@googlemail.com>
2019-04-16 20:39:10 +0200

    Switching to Flutter setup

---
+++ android/app/src/main/java/sibby/puns/de/jamoketra/MainActivity.java
@@ -0,0 +1,13 @@
+package sibby.puns.de.jamoketra;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
