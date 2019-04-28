5ba39569540258f7374f7a3e0d6750791cf62e80
Simon Hampe <simon.hampe@googlemail.com>
2019-04-28 16:08:03 +0200

    Getting started on generator

---
+++ lib/kana/KanaType.dart
@@ -0,0 +1,12 @@
+class KanaType{
+
+  final _characters;
+
+  const KanaType._internal(this._characters);
+
+  get characters => _characters;
+
+  static const HIRAGANA = KanaType._internal("あいうえおかがきぎくぐけげこごさざしじすずせぜそぞただちぢつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもやゆよらりるれろわをん");
+  static const KATAKANA = KanaType._internal("アイウエオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモヤユヨラリルレロワヲン");
+
+}
---
+++ lib/kana/RandomKanaGenerator.dart
@@ -0,0 +1,15 @@
+import 'package:jamoketra/kana/KanaType.dart';
+
+class RandomKanaGenerator {
+
+  final int minLength;
+  final int maxLength;
+  final KanaType type;
+
+  RandomKanaGenerator(this.minLength, this.maxLength, this.type);
+
+  String generate() {
+    
+  }
+
+}
